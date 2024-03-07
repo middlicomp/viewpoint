f77a14b852245c529419ed81e93462b74994f9c4
mnyrop <mnyrop@users.noreply.github.com>
2024-03-07 16:27:59 +0000

    deploy: 0c5673807a148e5032f68de8af76c8e0bcc6ce8a

---
+++ _app/immutable/chunks/paths.DkYGGm3P.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_z53iu6)==null?void 0:s.base)??"/viewpoint";var e;const a=((e=globalThis.__sveltekit_z53iu6)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.BeTtDuji.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D1kpY8eK.js";export{t as start};
